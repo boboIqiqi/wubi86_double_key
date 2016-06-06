75161009cb8725c3354fd6707f8df4522699038f
boboIqiqi <quietwave@163.com>
2016-06-06 22:29:38 +0800

    实现了逗号选第3候选字，句号选第2候选字。 添加debug文件夹，用来调试

---
+++ debug/get.bat
@@ -0,0 +1,6 @@
+adb pull /sdcard/rime/trime.yaml
+adb pull /sdcard/rime/trime.custom.yaml
+adb pull /sdcard/rime/wubi86_double_key.schema.yaml
+adb pull /sdcard/rime/default.custom.yaml
+adb pull /sdcard/rime/default.yaml
+pause
