8fd4e4e52e28e175b2170aad34ffe06a10c32fac
boboIqiqi <quietwave@163.com>
2016-06-06 21:20:18 +0800

    首次提交，还有很多问题

---
+++ get.bat
@@ -0,0 +1,4 @@
+adb pull /sdcard/rime/trime.yaml
+adb pull /sdcard/rime/trime.custom.yaml
+adb pull /sdcard/rime/wubi86_double_key.schema.yaml
+pause
---
+++ trime.bat
@@ -0,0 +1,5 @@
+adb push trime.custom.yaml /sdcard/rime
+adb push wubi86_double_key.schema.yaml /sdcard/rime
+adb push wubi86_double_key.dict.yaml /sdcard/rime
+adb shell am broadcast -a com.osfans.trime.deploy
+pause
